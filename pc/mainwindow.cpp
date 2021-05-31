ec4ec40568a85ca506d24736a3409d2eb3ae9037
Benjamin Feller <benjamin.feller@epitech.eu>
2021-05-31 23:01:32 +0200

    Shit code!

--- pc/mainwindow.cpp
+++ pc/mainwindow.cpp
@@ -1,15 +1,72 @@
-#include "../include/mainwindow.h"
+#include <QMessageBox>
+#include <QJsonValue>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include "mainwindow.h"
 #include "../ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    , ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
+
+void MainWindow::replyFinished(QNetworkReply *reply) {
+    qDebug() << reply->attribute(QNetworkRequest::HttpReasonPhraseAttribute ).toString();
+    if (reply->error()) {
+    qDebug() << reply->errorString();
+    return;
+    }
+
+    QString answer = reply->readAll();
+    qDebug() << answer;
+}
+
+void MainWindow::on_signinSubmit_clicked() {
+    QString email = ui->emailInput->text();
+    QString password = ui->passwordInput->text();
+
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    connect(manager, &QNetworkAccessManager::finished,
+            this, [=](QNetworkReply *reply){
+
+
+            qDebug() << reply->attribute(QNetworkRequest::HttpReasonPhraseAttribute ).toString();
+            if (reply->error()) {
+            qDebug() << reply->errorString();
+            ui->loginErrorLabel->setText(reply->errorString());
+
+            return;
+        }
+
+        QString answer = reply->readAll();
+        QByteArray br = answer.toUtf8();
+
+        QJsonDocument doc = QJsonDocument::fromJson(br);
+        QJsonObject obj = doc.object();
+
+        QJsonValue val(answer);
+        QJsonObject lol = val.toObject();
+
+         QMessageBox::information(this, "Login", obj["name"].toString());
+        qDebug() << answer;
+    });
+
+    QNetworkRequest req = QNetworkRequest(QUrl("http://localhost:8000/api/auth/login"));
+    req.setHeader(QNetworkRequest::KnownHeaders::ContentTypeHeader, "application/json;charset=UTF-8");
+
+    QString pute = QString("{\"email\":\"") + email + QString("\",\"password\":\"") + password + QString("\"}");
+    manager->post(req, pute.toStdString());
+
+    if(email == "test" && password == "test") {
+        ui->loginErrorLabel->setText("Error");
+    } else {
+        QMessageBox::warning(this, "Login", "Bad email or password");
+    }
+}
+
 
--- pc/mainwindow.h
+++ pc/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_signinSubmit_clicked();
+
 private:
     Ui::MainWindow *ui;
+    void replyFinished(QNetworkReply *reply);
 };
 #endif // MAINWINDOW_H
